5e08dcf914ac7cd63ec960002db172129c82f6d2
Omelchenko Michael <socanime@gmail.com>
2023-02-23 02:37:34 +0300

    fix git

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -30,10 +30,6 @@ export default function Home() {
           Curabitur turpis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc,
           vitae euismod ligula urna in dolor. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit
           id, lorem. Phasellus blandit leo ut odio. Vestibulum ante ipsum primis in faucibus orci
-          luctus et ultrices posuere cubilia Curae; Fusce id purus. Aliquam lorem ante, dapibus in,
-          viverra quis, feugiat a, tellus. In consectetuer turpis ut velit. Aenean posuere, tortor
-          sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus.
-          Vestibulum suscipit nulla quis orci. Nam commodo suscipit quam. Sed a libero.
         </Typography>
 
       </Container>
